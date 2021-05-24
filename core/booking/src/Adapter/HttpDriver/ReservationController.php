1c32a5c8a978038a9c8b92d19fcc9229c922c93e
zero <teclaizerai@gmail.com>
2021-05-24 12:37:10 +0200

    Setup-client form.

--- core/booking/src/Adapter/HttpDriver/ReservationController.php
+++ core/booking/src/Adapter/HttpDriver/ReservationController.php
@@ -2,6 +2,7 @@
 
 namespace Booking\Adapter\HttpDriver;
 
+use Booking\Infrastructure\Framework\Form\ClientType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,8 +10,11 @@ class ReservationController extends AbstractController
 {
     public function __invoke(): Response
     {
+        $form = $this->createForm(ClientType::class);
+
         return $this->render('Static/reservation.html.twig', [
             'page' => 'reservation',
+            'form' => $form->createView(),
         ]);
     }
 }
