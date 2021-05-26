3a821e9c17d988a6f1272babadb0084ae1364b80
zero <teclaizerai@gmail.com>
2021-05-26 11:41:34 +0200

    basic setup adozioni controller e form (single file).

--- core/booking/src/Adapter/HttpDriver/AdozioniController.php
+++ core/booking/src/Adapter/HttpDriver/AdozioniController.php
@@ -5,6 +5,8 @@ namespace Booking\Adapter\HttpDriver;
 use Booking\Infrastructure\Framework\Form\AdozioniReservationType;
 use LeanpubBookClub\Application\UpdateSession;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,12 +18,52 @@ class AdozioniController extends AbstractController
 
         $form->handleRequest($request);
 
+        //var_dump($request->files);
+
         if ($form->isSubmitted() && $form->isValid()) {
+
+############################ inizio single file upload
+            /** @var UploadedFile $adozioniFile */
+            $adozioniFile = $form->get('adozioni')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($adozioniFile) {
+                $originalFilename = pathinfo($adozioniFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $adozioniFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $adozioniFile->move(
+                        $this->getParameter('adozioni_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                    throw $e;
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                //$product->setBrochureFilename($newFilename);
+            }
+            var_dump($adozioniFile);
+
+            die;
+            ############################ fine single file upload
             $formData = $form->getData();
+
+            var_dump($formData);
+            die;
+
+            ############################ Execute Application/domain logic here
 
 //            $this->application->updateSession(
 //                new UpdateSession($sessionId, $formData['description'], $formData['urlForCall'])
 //            );
+            ############################ Execute Application/domain logic here
 
             return $this->redirectToRoute('home_oberdan');
         }
--- core/booking/src/Infrastructure/Framework/Form/AdozioniReservationType.php
+++ core/booking/src/Infrastructure/Framework/Form/AdozioniReservationType.php
@@ -3,9 +3,14 @@
 namespace Booking\Infrastructure\Framework\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class AdozioniReservationType extends AbstractType
 {
@@ -18,11 +23,45 @@ class AdozioniReservationType extends AbstractType
                 //                    'label' => false,
                 //                ],
             ])
+            ->add('adozioni', FileType::class, [
+                'label' => 'File delle adozioni (formato ?)',
 
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                'multiple' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5120k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ]),
+                ],
+            ])
             ->add('notes', TextareaType::class, [
                 'label' => 'Altre informazioni',
             ])
-        ;
+            ->add('privacyConfirmed', CheckboxType::class, [
+                'label' => 'Acconsento al trattamento dei dati secondo la normativa sulla privacy',
+                'required' => true,
+            ])
+            ->add(
+                'submit',
+                SubmitType::class,
+                [
+                    'label' => 'Invia',
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
