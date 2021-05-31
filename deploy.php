6e6f93f65393109229b6398513374c5941fc5eb3
zero <teclaizerai@gmail.com>
2021-05-31 16:46:42 +0200

    Deployer: configurazione per stage environment.

--- deploy.php
+++ deploy.php
@@ -3,13 +3,14 @@ namespace Deployer;
 
 require 'recipe/symfony4.php';
 
-set('default_stage', 'test');
+set('default_stage', 'stage');
 
 // Project name
 set('application', 'stage.oberdan8.it');
 
 // Project repository
-set('repository', 'https://github.com/zerai/oberdan8.git');
+//set('repository', 'https://github.com/zerai/oberdan8.git');
+set('repository', 'git@github.com:zerai/oberdan8.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
@@ -21,20 +22,36 @@ add('shared_dirs', []);
 // Writable dirs by web server 
 add('writable_dirs', []);
 
+// Application path
+set('application_path_stage', 'stage.oberdan8.it');
+
+set('application_path_prod', 'prenota.oberdan8.it');
+
+
+
+
 
 // Hosts
 
 //host('project.com')
 //    ->set('deploy_path', '~/{{application}}');
 
-host('test')
+host('stage')
     ->hostname('oberdan8.it')
     ->user('oberdani')
-    ->port(22)
+    ->port(3508)
     ->stage('test')
     ->set('branch', 'main')
-    ->set('deploy_path', '~/{{application}}')
+    ->set('deploy_path', '~/{{application_path_stage}}')
     ->set('identityFile', 'var/Oberdan/id_rsa_zerai_dev_machine')
+    ->set('http_user', 'oberdani')
+    ->set('writable_use_sudo', false)
+    ->set('writable_mode', 'chmod')
+
+    ->set('git_tty', false)
+    ->set('ssh_multiplexing', false)
+    ->set('composer_options', '{{composer_action}} --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader')
+    //->set('keep_releases', 10)
     ;
 
 
@@ -45,10 +62,16 @@ task('build', function () {
     run('cd {{release_path}} && build');
 });
 
+// Fake Task
+task('pwd', function () {
+    $result = run('pwd');
+    writeln("Current dir: $result");
+});
+
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-before('deploy:symlink', 'database:migrate');
+//before('deploy:symlink', 'database:migrate');
 
