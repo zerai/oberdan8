e3f5a3bed99e732107930465e6ae27262771fa5b
zero <teclaizerai@gmail.com>
2021-06-03 01:59:44 +0200

    test mail conf.

--- core/booking/src/Adapter/HttpDriver/ReservationController.php
+++ core/booking/src/Adapter/HttpDriver/ReservationController.php
@@ -2,25 +2,43 @@
 
 namespace Booking\Adapter\HttpDriver;
 
+use Booking\Infrastructure\Framework\Form\Dto\ReservationFormModel;
 use Booking\Infrastructure\Framework\Form\ReservationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ReservationController extends AbstractController
 {
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ReservationType::class);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var ReservationFormModel $formData */
             $formData = $form->getData();
 
 //            $this->application->updateSession(
 //                new UpdateSession($sessionId, $formData['description'], $formData['urlForCall'])
 //            );
+
+
+            //dd($formData);
+            $email = (new Email())
+                ->from('memu.system@medicalmundi.com')
+                ->to($formData->person->email)
+                ->subject('Nuova Prenotazione')
+                ->text('
+                Dati prenotazione \n
+                \n
+                Cognome: ' . $formData->person->lastName . '
+                ');
+
+            $mailer->send($email);
 
             $this->addFlash('success', 'Prenotazine avvenuta con successo.');
 
