b10189f77f6f58c47bb0dc0e34581582fb0fe7f1
zero <teclaizerai@gmail.com>
2021-06-17 12:25:21 +0200

    Added fixtures for stage env. (librai server).

---
+++ src/DataFixtures/LibraiStage/BackofficeAdminUserFixtures.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace App\DataFixtures\LibraiStage;
+
+use App\Entity\BackofficeUser;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class BackofficeAdminUserFixtures extends Fixture implements FixtureGroupInterface
+{
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new BackofficeUser();
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword(
+            $this->passwordEncoder->encodePassword($admin, 'demo')
+        );
+
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['stage', 'stageSecurity', 'stageRegularUser'];
+    }
+}
