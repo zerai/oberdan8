c0c2b13573cd441826f392bc2642f976e549fd9e
zero <teclaizerai@gmail.com>
2021-06-20 11:33:27 +0200

    [Booking] refatoring test folder.

--- tests/Functional/AdozioniReservation/AdozioniReservationTest.php
+++ tests/Functional/AdozioniReservation/AdozioniReservationTest.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
 
-namespace App\Tests\Functional;
+namespace App\Tests\Functional\AdozioniReservation;
 
 use PHPUnit\Framework\TestCase;
 
--- tests/Functional/RegularReservation/MailSendingInReservationControllerTest.php
+++ tests/Functional/RegularReservation/MailSendingInReservationControllerTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Tests\Functional;
+namespace App\Tests\Functional\RegularReservation;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
