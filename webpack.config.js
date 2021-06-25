415ec40a6f9704aef253b1e2312148a75b282eb7
zero <teclaizerai@gmail.com>
2021-06-25 12:09:06 +0200

    [UI] fix jquery import in reservation form.

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ Encore
      */
     .addEntry('app', './assets/app.js')
     .addEntry('backoffice', './assets/backoffice.js')
+    .addEntry('reservation_page', './assets/js/app/reservation_page.js')
     .addStyleEntry('email', './assets/styles/mailer/email.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
