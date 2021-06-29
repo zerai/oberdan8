5cdc226f3ad70fa6eebf5dc70bd66d092fdcf494
zero <teclaizerai@gmail.com>
2021-06-29 08:15:22 +0200

    [Booking] added reseration fixture (for stage).

--- src/DataFixtures/LibraiStage/ReservationFixtures.php
+++ src/DataFixtures/LibraiStage/ReservationFixtures.php
@@ -7,17 +7,9 @@ use App\Factory\ReservationFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ReservationFixtures extends Fixture implements FixtureGroupInterface
 {
-//    private UserPasswordEncoderInterface $passwordEncoder;
-//
-//    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
-//    {
-//        $this->passwordEncoder = $passwordEncoder;
-//    }
-
     public function load(ObjectManager $manager): void
     {
         $reservation = ReservationFactory::createOne([
@@ -27,10 +19,6 @@ class ReservationFixtures extends Fixture implements FixtureGroupInterface
         $reservations = ReservationFactory::createMany(50, [
             'books' => BookFactory::new()->many(1, 8),
         ]);
-
-        //$manager->persist($reservation);
-
-        //$manager->flush();
     }
 
     public static function getGroups(): array
