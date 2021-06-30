f5fc598ad5c6c9c5c191d4c4b565ceecb7cbc81c
zero <teclaizerai@gmail.com>
2021-06-30 18:25:12 +0200

    Added infoBox entity.

---
+++ src/Entity/InfoBox.php
@@ -0,0 +1,113 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\InfoBoxRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=InfoBoxRepository::class)
+ */
+class InfoBox
+{
+    public const INFO_BOX_TYPES = [
+        'none',
+        'info',
+        'warning',
+        'danger',
+    ];
+
+    /**
+     * @var UuidInterface
+     *
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class=UuidGenerator::class)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private string $body;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private bool $active;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private string $boxType = 'none';
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getBoxType(): ?string
+    {
+        return $this->boxType;
+    }
+
+    public function setBoxType(string $boxType): self
+    {
+        if (! \in_array($boxType, self::INFO_BOX_TYPES)) {
+            throw new \InvalidArgumentException('Invalid Message box type');
+        }
+
+        $this->boxType = $boxType;
+
+        return $this;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+}
---
+++ src/Repository/InfoBoxRepository.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\InfoBox;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method InfoBox|null find($id, $lockMode = null, $lockVersion = null)
+ * @method InfoBox|null findOneBy(array $criteria, array $orderBy = null)
+ * @method InfoBox[]    findAll()
+ * @method InfoBox[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class InfoBoxRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, InfoBox::class);
+    }
+
+    // /**
+    //  * @return InfoBox[] Returns an array of InfoBox objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('i.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?InfoBox
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
