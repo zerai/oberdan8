3a6ddc37fee6b3fbd67596384209d07f6c59fc81
zero <teclaizerai@gmail.com>
2021-07-12 07:20:38 +0200

    [Backoffice] fix backoffice ClientType form.

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use Booking\Infrastructure\Framework\Form\Dto\ClientDto;
+use App\Form\Dto\ClientDto;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
