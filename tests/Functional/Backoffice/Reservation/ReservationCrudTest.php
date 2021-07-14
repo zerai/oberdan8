ca2f72df7d4319e7732ee05c46f5e48a672cd5b4
zero <teclaizerai@gmail.com>
2021-07-14 23:57:48 +0200

    [Backoffice] refactoring tests folder.

--- tests/Functional/Backoffice/Reservation/ReservationCrudTest.php
+++ tests/Functional/Backoffice/Reservation/ReservationCrudTest.php
@@ -5,7 +5,6 @@ namespace App\Tests\Functional\Backoffice\Reservation;
 
 use App\Factory\ReservationFactory;
 use App\Tests\Functional\SecurityWebtestCase;
-use Booking\Application\Domain\Model\Reservation;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
@@ -89,107 +88,5 @@ class ReservationCrudTest extends SecurityWebtestCase
         self::assertResponseIsSuccessful();
 
         self::assertSelectorExists('.pagination');
-    }
-
-    /**
-     * @test
-     * @dataProvider changeReservationStatusDataProvider
-     */
-    public function shouldEditTheReservationStatusForAReservation(string $newStatus, string $expectedStatusInPage): void
-    {
-        /** @var Reservation $reservation */
-        $reservation = ReservationFactory::createOne()->object();
-
-        $this->logInAsAdmin();
-
-        $this->client->request('GET', '/admin/prenotazioni/' . $reservation->getId()->toString() . '/edit');
-
-        self::assertResponseIsSuccessful();
-
-        $this->client->followRedirects(true);
-
-        $crawler = $this->client->submitForm('Invia', [
-            'backoffice_reservation_edit[status]' => $newStatus,
-        ]);
-
-        self::assertResponseIsSuccessful();
-
-        self::assertGreaterThan(
-            0,
-            $crawler->filter("html div.card-status-prenotazione:contains(\"${expectedStatusInPage}\")")->count()
-        );
-    }
-
-    public function changeReservationStatusDataProvider(): \Generator
-    {
-        return [
-            yield 'InProgress' => ['InProgress', 'In lavorazione'],
-            yield 'Pending' => ['Pending', 'In sospeso'],
-            yield 'Rejected' => ['Rejected', 'Rifiutato'],
-            yield 'Confirmed' => ['Confirmed', 'Confermato'],
-            yield 'Sale' => ['Sale', 'Vendita'],
-            yield 'PickedUp' => ['PickedUp', 'Ritirato'],
-            yield 'Blacklist' => ['Blacklist', 'Blacklist'],
-        ];
-    }
-
-    /** @test */
-    public function shouldEditTheReservationNotesForAReservation(): void
-    {
-        /** @var Reservation $reservation */
-        $reservation = ReservationFactory::createOne()->object();
-
-        $this->logInAsAdmin();
-
-        $this->client->request('GET', '/admin/prenotazioni/' . $reservation->getId()->toString() . '/edit');
-
-        self::assertResponseIsSuccessful();
-
-        $this->client->followRedirects(true);
-
-        $this->client->submitForm('Invia', [
-            'backoffice_reservation_edit[notes]' => 'A new notes.',
-        ]);
-
-        self::assertResponseIsSuccessful();
-
-        $crawler = $this->client->request('GET', '/admin/prenotazioni/' . $reservation->getId()->toString());
-
-        self::assertResponseIsSuccessful();
-
-        self::assertGreaterThan(
-            0,
-            $crawler->filter('html div.card:contains("A new notes.")')->count()
-        );
-    }
-
-    /** @test */
-    public function shouldEditTheReservationPackageIdForAReservation(): void
-    {
-        /** @var Reservation $reservation */
-        $reservation = ReservationFactory::createOne()->object();
-
-        $this->logInAsAdmin();
-
-        $this->client->request('GET', '/admin/prenotazioni/' . $reservation->getId()->toString() . '/edit');
-
-        self::assertResponseIsSuccessful();
-
-        $this->client->followRedirects(true);
-
-        $this->client->submitForm('Invia', [
-            'backoffice_reservation_edit[packageId]' => 'B-23',
-        ]);
-
-        self::assertResponseIsSuccessful();
-
-        $crawler = $this->client->request('GET', '/admin/prenotazioni/' . $reservation->getId()->toString());
-
-        self::assertResponseIsSuccessful();
-
-        self::assertGreaterThan(
-            0,
-            $crawler->filter('html div.card:contains("B-23")')->count()
-        );
     }
 }
