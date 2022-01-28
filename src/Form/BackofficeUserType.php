8f8929cce6fa7de2f61494e06857572417f6d5b6
zerai <teclaizerai@gmail.com>
2022-01-28 13:33:01 +0100

    Update BackoffieUserFormType (Not blank on email).

--- src/Form/BackofficeUserType.php
+++ src/Form/BackofficeUserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\BackofficeUser;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -15,7 +16,13 @@ class BackofficeUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Inserisci una email',
+                    ]),
+                ],
+            ])
             //->add('roles')
             //->add('password')
             ->add('plainPassword', PasswordType::class, [
