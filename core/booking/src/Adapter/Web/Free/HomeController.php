68976b9afcb38f1ab96f72b62087c2807d4c908a
zerai <teclaizerai@gmail.com>
2022-04-30 13:09:55 +0200

    Refactoring HomeController.

--- core/booking/src/Adapter/Web/Free/HomeController.php
+++ core/booking/src/Adapter/Web/Free/HomeController.php
@@ -1,10 +1,14 @@
 <?php declare(strict_types=1);
 
-namespace Booking\Adapter\HttpDriver;
+namespace Booking\Adapter\Web\Free;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/", name="app_home", methods="GET")
+ */
 class HomeController extends AbstractController
 {
     public function __invoke(): Response
