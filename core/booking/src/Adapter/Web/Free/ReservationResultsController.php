286d8d0b59618c2c5337ab63c63fd8924f6c5a2c
zerai <teclaizerai@gmail.com>
2022-04-30 19:55:26 +0200

    Refactoring ReservationResultsController.

--- core/booking/src/Adapter/Web/Free/ReservationResultsController.php
+++ core/booking/src/Adapter/Web/Free/ReservationResultsController.php
@@ -1,11 +1,15 @@
 <?php declare(strict_types=1);
 
-namespace Booking\Adapter\HttpDriver;
+namespace Booking\Adapter\Web\Free;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/esito", name="reservation_result", methods={"GET"})
+ */
 class ReservationResultsController extends AbstractController
 {
     public function __invoke(Request $request): Response
