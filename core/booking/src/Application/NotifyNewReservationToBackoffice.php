47f4ec91bf590a715c469b34a185a4685bf4006c
zero <teclaizerai@gmail.com>
2024-06-25 13:26:13 +0200

    Cleanup code.

--- core/booking/src/Application/NotifyNewReservationToBackoffice.php
+++ core/booking/src/Application/NotifyNewReservationToBackoffice.php
@@ -6,5 +6,5 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 
 interface NotifyNewReservationToBackoffice extends ApplicationPort
 {
-    public function notifyNewReservationToBackoffice(array $personData, array $bookData, array $systemData = [], string $otherInfo): TemplatedEmail;
+    public function notifyNewReservationToBackoffice(array $personData, array $bookData, array $systemData = [], string $otherInfo = '', string $coupondCode = ''): TemplatedEmail;
 }
