a0664046c76c95bd42b68aaaccdc96a99a1045b9
zero <teclaizerai@gmail.com>
2024-06-25 11:20:56 +0200

    Update test MailSendingInReservationControllerTest.php.

--- tests/Functional/RegularReservation/MailSendingInReservationControllerTest.php
+++ tests/Functional/RegularReservation/MailSendingInReservationControllerTest.php
@@ -167,4 +167,55 @@ class MailSendingInReservationControllerTest extends WebTestCase
         self::assertEmailTextBodyContains($email, ReservationStaticFixture::PHONE);
         self::assertEmailAttachmentCount($email, 0);
     }
+
+    /**
+     * Test with coupond code
+     */
+    /** @test */
+    public function coupond_code_afterFormSubmit_shouldSendTwoEmail(): void
+    {
+        $client = static::createClient();
+
+        $csrfToken = $client->getContainer()->get('security.csrf.token_manager')->getToken('reservation');
+
+        $client->request(
+            Request::METHOD_POST,
+            '/reservation',
+            [
+                'reservation' => [
+                    'person' => [
+                        "last_name" => ReservationStaticFixture::LAST_NAME,
+                        "first_name" => ReservationStaticFixture::FIRST_NAME,
+                        "email" => ReservationStaticFixture::EMAIL,
+                        "phone" => ReservationStaticFixture::PHONE,
+                        "city" => ReservationStaticFixture::CITY,
+                    ],
+                    'classe' => ReservationStaticFixture::CLASSE,
+                    'books' => [
+                        [
+                            "isbn" => ReservationStaticFixture::BOOK_ONE_ISBN,
+                            "title" => ReservationStaticFixture::BOOK_ONE_TITLE,
+                            "author" => ReservationStaticFixture::BOOK_ONE_AUTHOR,
+                            "volume" => ReservationStaticFixture::BOOK_ONE_VOLUME,
+                        ],
+                        [
+                            "isbn" => ReservationStaticFixture::BOOK_TWO_ISBN,
+                            "title" => ReservationStaticFixture::BOOK_TWO_TITLE,
+                            "author" => ReservationStaticFixture::BOOK_TWO_AUTHOR,
+                            "volume" => ReservationStaticFixture::BOOK_TWO_VOLUME,
+                        ],                    ],
+                    "otherInfo" => "Vorrei sapere di che anno è la vostra edizione.",
+                    "coupondCode" => "ABCDEF",
+                    "privacyConfirmed" => "1",
+                    "submit" => "",
+                    "_token" => $csrfToken->getValue(),
+                ],
+            ],
+            [],
+        );
+
+        self::assertResponseRedirects(self::REDIRECT_AFTER_SUBMIT);
+
+        self::assertEmailCount(2);
+    }
 }
