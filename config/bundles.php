fc5812b7d57172e69fad5c8de8d634e8611d9014
zero <teclaizerai@gmail.com>
2025-05-19 13:57:22 +0200

    Composer: update recipe sentry/sentry-symfony. (#462)
    * Composer: update recipe sentry/sentry-symfony.
    
    * Fix sentry conf.
    
    * Fix sentry conf.
    
    * Fix sentry conf.

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true, 'dev' => true, 'test' => true],
 ];
