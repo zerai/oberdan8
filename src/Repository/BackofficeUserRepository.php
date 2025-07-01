736fbf396f5bd93a1b0adcd58298e1cdaf28f82f
zero <teclaizerai@gmail.com>
2025-07-01 19:29:30 +0200

    Fix CS.

--- src/Repository/BackofficeUserRepository.php
+++ src/Repository/BackofficeUserRepository.php
@@ -8,7 +8,6 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @method BackofficeUser|null find($id, $lockMode = null, $lockVersion = null)
