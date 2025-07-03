47f357d379ec749677d10998d80e3a066651fd32
zero <teclaizerai@gmail.com>
2025-07-03 11:31:19 +0200

    Fix SA.

--- tests/Functional/SecurityWebtestCase.php
+++ tests/Functional/SecurityWebtestCase.php
@@ -7,7 +7,6 @@ use App\Repository\BackofficeUserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
 use function Zenstruck\Foundry\factory;
 
 class SecurityWebtestCase extends WebTestCase
@@ -45,53 +44,5 @@ class SecurityWebtestCase extends WebTestCase
     protected function logInAsAdmin(): void
     {
         $this->newLogInAsAdmin();
-        //$this->realLogInAsAdmin();
-        //        $backofficeUserRepository = $this->client->getContainer()->get(BackofficeUserRepository::class);
-        //
-        //        $admin = $backofficeUserRepository->findOneBy([
-        //            'email' => 'admin@example.com',
-        //        ]);
-        //
-        //        if (null === $admin) {
-        //            // find a persisted object for the given attributes, if not found, create with the attributes
-        //            $factory = factory(BackofficeUser::class);
-        //
-        //            #TODO: remove deprecated code
-        //            //            $admin = $factory->findOrCreate([
-        //            //                //  'id' => 100,
-        //            //                'email' => 'admin@example.com',
-        //            //                'active' => true,
-        //            //                'password' => 'xxx',
-        //            //                'roles' => ['ROLE_ADMIN'],
-        //            //            ])->object();
-        //
-        //            $admin = $factory->findOrCreate([
-        //                //  'id' => 100,
-        //                'email' => 'admin@example.com',
-        //                'active' => true,
-        //                'password' => 'xxx',
-        //                'roles' => ['ROLE_ADMIN'],
-        //            ])->_real();
-        //        }
-        //
-        //        $session = $this->client->getContainer()->get('session');
-        //
-        //        $firewallName = 'secure_area';
-        //        // if you don't define multiple connected firewalls, the context defaults to the firewall name
-        //        // See https://symfony.com/doc/current/reference/configuration/security.html#firewall-context
-        //        $firewallContext = 'secured_area';
-        //
-        //        // you may need to use a different token class depending on your application.
-        //        // for example, when using Guard authentication you must instantiate PostAuthenticationGuardToken
-        //
-        //        //$token = new UsernamePasswordToken('admin@example.com', null, $firewallName, ['ROLE_ADMIN']);
-        //
-        //        $token = new PostAuthenticationGuardToken($admin, 'app_user_provider', $admin->getRoles());
-        //
-        //        $session->set('_security_' . $firewallContext, serialize($token));
-        //        $session->save();
-        //
-        //        $cookie = new Cookie($session->getName(), $session->getId());
-        //        $this->client->getCookieJar()->set($cookie);
     }
 }
