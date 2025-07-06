985cceef14563d4ecd4c1f4adf82457e5fdde312
zero <teclaizerai@gmail.com>
2025-07-06 17:28:55 +0200

    Added google recaptcha.
    - composer req beelab/recaptcha2-bundle
    - bundle config

--- core/booking/src/Adapter/Web/Free/Form/ReservationType.php
+++ core/booking/src/Adapter/Web/Free/Form/ReservationType.php
@@ -2,6 +2,9 @@
 
 namespace Booking\Adapter\Web\Free\Form;
 
+use Beelab\Recaptcha2Bundle\Form\Type\RecaptchaSubmitType;
+use Beelab\Recaptcha2Bundle\Form\Type\RecaptchaType;
+use Beelab\Recaptcha2Bundle\Validator\Constraints\Recaptcha2;
 use Booking\Adapter\Web\Free\Form\Dto\ReservationFormModel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -63,6 +66,20 @@ class ReservationType extends AbstractType
             ->add('privacyConfirmed', CheckboxType::class, [
                 'label' => 'Acconsento al trattamento dei dati secondo la normativa sulla privacy',
                 'required' => true,
+            ])
+
+//            ->add('captcha', RecaptchaType::class, [
+//                // You can use RecaptchaSubmitType
+//                // "groups" option is not mandatory
+//                //'constraints' => new Recaptcha2(['groups' => ['create']]),
+//            ])
+            // For Invisible Recaptcha
+
+            ->add('captcha', RecaptchaSubmitType::class, [
+                'constraints' => new Recaptcha2([
+                    'groups' => ['RegularReservation', 'AdozioniReservation'],
+                ]),
+                //'label' => 'Save'
             ])
 
             ->add(
